396bd1df4ea85e70d87ec0b0feafe50244389ab5
PG Herveou <pgherveou@lyft.com>
2022-03-24 16:10:46 +0100

    fix / simplify files

--- hello-bin.js
+++ hello-bin.js
@@ -1,6 +1,2 @@
-const fs = require('fs')
-const { inspect } = require('util')
 const { greeting } = require('./hello_world_nodejs_wasm_bindgen')
-
 console.log(greeting('PG'))
-// fs.writeFileSync(process.argv[2], `${greeting('PG')}`, 'utf8')
